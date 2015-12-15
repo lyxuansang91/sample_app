8f990d4be326ac661c5291cea76fd9c94191bd59
Ly Xuan Sang <lyxuansang1991@gmail.com>
2015-12-15 18:44:19 +0700

    add Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    # byebug
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      flash[:danger] = "Sign up error!"
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.required(:user).permit(:name,
+      :email,
+      :password,
+      :password_confirmation)
   end
 end
